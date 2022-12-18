b99b981b817f21f7e17d5fe9f5a3deba39948361
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:40:13 +0300

    Update tb_davranissal_toplayici.v

--- testbench/yontem2/tb_davranissal_toplayici.v
+++ testbench/yontem2/tb_davranissal_toplayici.v
@@ -4,24 +4,17 @@
 module tb_davranissal_toplayici(
 
     );
-    reg [63:0] number1;
-    reg [63:0] number2;
-    reg [64:0] sum;
+     reg [151:0] packet;
+    wire [64:0] sum;
     
-    davranissal_toplayici uut(number1,number2,sum);
+    davranissal_toplayici uut(packet,sum);
     initial begin
         
-    number1 = 64'd1; number2 = 64'd1; #5;
-    number1 = 64'd1; number2 = 64'd2; #5;
-    number1 = 64'd1; number2 = 64'd3; #5;
-    number1 = 64'd1; number2 = 64'd4; #5;
-    number1 = 64'd1; number2 = 64'd50; #5;
-    number1 = 64'd1; number2 = 64'd55; #5;
-    number1 = 64'd10; number2 = 64'd10; #5;
-    number1 = 64'd15; number2 = 64'd30; #5;
-    number1 = 64'd25; number2 = 64'd52; #5;
-    number1 = 64'd60; number2 = 64'd17; #5;
-    
+    packet = 152'd100;#5;
+    packet = 152'd50;#5;
+    packet = 152'd25;#5;
+    packet = 152'd36;#5;
+    packet = 152'd72;#5;
     end
     
 endmodule
