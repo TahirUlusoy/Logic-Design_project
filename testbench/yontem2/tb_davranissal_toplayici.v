036e9c1ed04c4f6782d531d2faf92e87f03612e7
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 13:30:10 +0300

    Update tb_davranissal_toplayici.v

--- testbench/yontem2/tb_davranissal_toplayici.v
+++ testbench/yontem2/tb_davranissal_toplayici.v
@@ -11,10 +11,16 @@ module tb_davranissal_toplayici(
     davranissal_toplayici uut(number1,number2,sum);
     initial begin
         
-        number1=64'd1; number2=64'd2; #5;
-        number1=64'd1; number2=64'd3; #5;
-        number1=64'd1; number2=64'd4; #5;
-        number1=64'd1; number2=64'd5; #5;
+    number1 = 64'd1; number2 = 64'd1; #5;
+    number1 = 64'd1; number2 = 64'd2; #5;
+    number1 = 64'd1; number2 = 64'd3; #5;
+    number1 = 64'd1; number2 = 64'd4; #5;
+    number1 = 64'd1; number2 = 64'd50; #5;
+    number1 = 64'd1; number2 = 64'd55; #5;
+    number1 = 64'd10; number2 = 64'd10; #5;
+    number1 = 64'd15; number2 = 64'd30; #5;
+    number1 = 64'd25; number2 = 64'd52; #5;
+    number1 = 64'd60; number2 = 64'd17; #5;
     
     end
     
