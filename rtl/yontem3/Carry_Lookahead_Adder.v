4d8bdfd6353e5b1ba5dd8c30ee400d3d61cd66c3
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:38:31 +0300

    Update Carry_Lookahead_Adder.v

--- rtl/yontem3/Carry_Lookahead_Adder.v
+++ rtl/yontem3/Carry_Lookahead_Adder.v
@@ -2,40 +2,35 @@
 
 
 module Carry_Lookahead_Adder(
-
-
-input [31:0] A,
-input [31:0] B,
-input cin,
-output [31:0] sum,
-output cout
+    input [151:0] packet,
+    output [63:0] sum,
+    output cout
 );
+    wire [63:0] A = packet[135:72];
+    wire [63:0] B = packet[71:8];
 
 
-
-wire [31:0] k;
-wire [31:0] m;
-wire [32:0] c;
+wire [63:0] k;
+wire [63:0] m;
+wire [64:0] c;
 
 genvar j, i;
 generate
 
- assign c[0] = cin;
-
- for(j = 0; j < 32; j = j + 1)
- begin
- assign k[j] = A[j] & B[j];
- assign m[j] = A[j] | B[j];
- assign c[j+1] = k[j] | m[j] & c[j];
- end
-
- assign cout = c[32];
-
- for(i = 0; i < 32; i = i+1)
- begin
- assign sum[i] = A[i] ^ B[i] ^ c[i];
- end
-endgenerate
+     for(j = 0; j < 64; j = j + 1)
+     begin
+     assign k[j] = A[j] & B[j];
+     assign m[j] = A[j] | B[j];
+     assign c[j+1] = k[j] | m[j] & c[j];
+     end
+    
+     assign cout = c[64];
+    
+     for(i = 0; i < 64; i = i+1)
+     begin
+     assign sum[i] = A[i] ^ B[i] ^ c[i];
+     end
+    endgenerate
 
 
 endmodule
