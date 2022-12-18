854c30505a027952a6dcb30f7100620c167b30ba
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 13:29:44 +0300

    Update tb_ripple_carry_adder.v

--- testbench/yontem1/tb_ripple_carry_adder.v
+++ testbench/yontem1/tb_ripple_carry_adder.v
@@ -13,9 +13,16 @@ module tb_ripple_carry_adder(
     ripple_carry_adder uut(number1,number2,sum,Co);
     
     initial begin
-    number1 = 64'd1; number2 = 64'd1; #5;
-    number1 = 64'd50; number2 = 64'd50; #5;
-    number1 = 64'd25; number2 = 64'd25; #5;
+   number1 = 64'd1; number2 = 64'd1; #5;
+    number1 = 64'd1; number2 = 64'd2; #5;
+    number1 = 64'd1; number2 = 64'd3; #5;
+    number1 = 64'd1; number2 = 64'd4; #5;
+    number1 = 64'd1; number2 = 64'd50; #5;
+    number1 = 64'd1; number2 = 64'd55; #5;
+    number1 = 64'd10; number2 = 64'd10; #5;
+    number1 = 64'd15; number2 = 64'd30; #5;
+    number1 = 64'd25; number2 = 64'd52; #5;
+    number1 = 64'd60; number2 = 64'd17; #5;
     
     end
 endmodule
