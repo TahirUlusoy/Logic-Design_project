5806e6fc2a0f7e9a3b5356016b0153d868c566c7
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:39:53 +0300

    Update tb_ripple_carry_adder.v

--- testbench/yontem1/tb_ripple_carry_adder.v
+++ testbench/yontem1/tb_ripple_carry_adder.v
@@ -5,24 +5,18 @@ module tb_ripple_carry_adder(
 
     );
     
-    reg [63:0] number1;
-    reg [63:0] number2; 
+    reg [151:0] packet;
     wire [63:0] sum;
     wire Co;
     
-    ripple_carry_adder uut(number1,number2,sum,Co);
+    ripple_carry_adder uut(packet,sum,Co);
     
     initial begin
-   number1 = 64'd1; number2 = 64'd1; #5;
-    number1 = 64'd1; number2 = 64'd2; #5;
-    number1 = 64'd1; number2 = 64'd3; #5;
-    number1 = 64'd1; number2 = 64'd4; #5;
-    number1 = 64'd1; number2 = 64'd50; #5;
-    number1 = 64'd1; number2 = 64'd55; #5;
-    number1 = 64'd10; number2 = 64'd10; #5;
-    number1 = 64'd15; number2 = 64'd30; #5;
-    number1 = 64'd25; number2 = 64'd52; #5;
-    number1 = 64'd60; number2 = 64'd17; #5;
+    packet = 152'd100;#5;
+    packet = 152'd50;#5;
+    packet = 152'd25;#5;
+    packet = 152'd36;#5;
+    packet = 152'd72;#5;
     
     end
 endmodule
