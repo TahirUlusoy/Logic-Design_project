9c3dd0014d91c10a6db169e1eca9c6e417fb7cc2
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:37:58 +0300

    Update ripple_carry_adder.v

--- rtl/yontem1/ripple_carry_adder.v
+++ rtl/yontem1/ripple_carry_adder.v
@@ -2,14 +2,14 @@
 
 
 module ripple_carry_adder (
-    input [63:0] number1,
-    input [63:0] number2, 
+    input [151:0] packet, 
     output [63:0] sum, 
     output Co
     );
     wire w[64:0];
     genvar i;
-
+    wire [63:0] number1 = packet[135:72];
+    wire [63:0] number2 = packet[71:8];
   
       for (i = 0; i < 64; i = i + 1) begin
         fulladder u(number1[i], number2[i], i == 0 ? 1'b0 : w[i-1], sum[i], w[i]);
