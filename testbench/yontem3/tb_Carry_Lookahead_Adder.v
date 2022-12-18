9d05b01318c4a4aa8a501e319ab8460a552592d5
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:40:37 +0300

    Update tb_Carry_Lookahead_Adder.v

--- testbench/yontem3/tb_Carry_Lookahead_Adder.v
+++ testbench/yontem3/tb_Carry_Lookahead_Adder.v
@@ -2,58 +2,22 @@
 
 module tb_Carry_Lookahead_Adder( );
 
- reg cintb;
- reg [31:0] Atb;
- reg [31:0] Btb;
- wire coutb;
- wire [31:0] sumtb;
+    reg [151:0] packet;
+    wire [63:0] sum;
+    wire cout;
 
 
-Carry_Lookahead_Adder uut(
-         .sum (sumtb[31:0]),
-         .cout (coutb),
-         .A (Atb[31:0]),
-         .B (Btb[31:0]),
-         .cin (cintb));
+Carry_Lookahead_Adder uut(packet,sum,cout);
 
-initial begin
-
-  cintb = 1'b0;
-  end
 
  initial begin
 
-   Atb = 32'd10;
-   Btb = 32'd10;
-    #100;
-    Atb = 32'd20;
-    Btb = 32'd0;
-    #100;
-     Atb = 32'd14;
-    Btb = 32'd13;
-    #100;
-    Atb = 32'd4;
-    Btb = 32'd6;
-    #100;
-    Atb = 32'd7;
-    Btb = 32'd1;
-    #100;
-    Atb = 32'd2;
-   Btb = 32'd4;
-    #100;
-     Atb = 32'd6;
-    Btb = 32'd13;
-    #100;
-    Atb = 32'd11;
-    Btb = 32'd12;
-    #100;
-     #100;
-     Atb = 32'd8;
-    Btb = 32'd11;
-    #100;
-    Atb = 32'd6;
-    Btb = 32'd3;
-    #100;
+    packet = 152'd100;#5;
+    packet = 152'd50;#5;
+    packet = 152'd25;#5;
+    packet = 152'd36;#5;
+    packet = 152'd72;#5;
+   
   end
 
    
