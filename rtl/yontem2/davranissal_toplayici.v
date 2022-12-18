8f9379fe72fd5aa9139be72b38344c4faffc5d26
TahirUlusoy <78096050+TahirUlusoy@users.noreply.github.com>
2022-12-18 19:39:23 +0300

    Update davranissal_toplayici.v

--- rtl/yontem2/davranissal_toplayici.v
+++ rtl/yontem2/davranissal_toplayici.v
@@ -2,11 +2,12 @@
 
 
 module davranissal_toplayici(
-    input[63:0] number1,
-    input [63:0] number2,
+    input [151:0] packet,
     output reg [64:0] sum
     );
     
+    wire [63:0] number1 = packet[135:72];
+    wire [63:0] number2 = packet[71:8];
     always@(*) begin
         sum = number1 + number2;
     end
